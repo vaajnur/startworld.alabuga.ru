457a13833718db974cab2bf2894f20893cb94f5b
AGValiev <AGValiev@alabuga.ru>
2025-03-24 07:23:43 +0300

    grant programm

---
+++ grant_program.php
@@ -0,0 +1,291 @@
+<div class="main-program grant-program">
+<div class="page__wrap">
+
+	<h2 class="text-center">"Alabuga Start" grant program</h2>
+
+	<div class="main-fields_block mb-10">
+	    <div class="d-flex flex-wrap justify-content-center">
+	        
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue ">
+	                        77 </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                            countries are taking part in the "Alabuga Start" program
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue">
+	                        > 60 </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                            representatives will be able to receive financial support from «SEZ SPT Alabuga» JSC for high involvement.
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx max-w-40" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue nowrap">
+	                        580,000 <span class="color-blue text-lg">USD</span> </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                            Total prize fund for grant competition participants
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	    </div>
+	</div>
+
+
+	<h2 class="text-center">Nominations for the "Alabuga Start" grant program</h2>
+
+
+	<div class="main-fields_block">
+	    <div class="d-flex flex-wrap justify-content-center mb-10">
+	        
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Incentive payout:</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                        	Payments for employment of candidates for the Alabuga Start program nomination:
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">
+	                        «Employment Leader»</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                        	Payments for collecting a full package of documents for registration of a candidate for the program
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">
+	                        «Full Support»</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                        	USD 7000 average payout to one representative for fulfillment of the conditions of two nominations
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	     </div>
+	    <div class="d-flex flex-wrap justify-content-center">
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">
+	                        Competitive payout:</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                        	A one-time payment to one representative per region for closing the largest percentage of established workplaces in the region, but no less than 70% in the shortest possible time
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">
+	                    	Partner of the Year
+	                        </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                        	Up to 14000 USD to one representative in the “Partner of the Year” nomination
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	    </div>
+	</div>
+
+
+	<p class="notes my-10">*Payout depends on the number of quotas that have been allocated to your country!</p>
+
+
+	<div class="d-flex flex-wrap">
+	    
+
+	    <div class="bx">
+	    	<div class="main-program_cart">
+	            <h2>
+	                Stages of the grant program </h2>
+	            <div class="list">
+	                <ol class="grant-list pl-0">
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/telegram.svg" alt="Professional training" class="mw-100 d-block">
+	                        </div>
+	                       Acceptance of applications for participation in the Grant Program 
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/laptop_icon.svg" alt="Russian language lessons" class="mw-100 d-block">
+	                        </div>
+	                        Recruitment process organized by representatives within the framework of the Grant Program
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/id-card_icon.svg" alt="Preferential accommodation in comfortable" class="mw-100 d-block">
+	                        </div>
+	                        Employment of candidates recruited by the representatives
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/exam-results_icon.svg" alt="Preferential accommodation in comfortable" class="mw-100 d-block">
+	                        </div>
+	                        Announcement of the results of the Grant Program
+	                    </li>
+	                </ol>
+	            </div>
+	        </div>	
+	    </div>
+
+
+	    <div class="bx">
+	        <div class="main-program_cart">
+	            <h2>
+	                Documents </h2>
+	                <p>For detailed information on the terms and nominations of the Grant Program, the number of quotas, conditions of participation and payouts, please refer to the Grant Program Regulations.</p>
+
+			        <div class="cart-fields_btn">
+		                <a target="_blank" href="/upload/regulations_on_the_grant_competition_alabuga_start.pdf" class="page__btn page__btn--curr">
+		                    <span>Documents</span>
+		                </a>
+		            </div>
+
+	        </div>
+	    </div>
+
+
+	</div>
+
+
+	<h2 class="my-10">Submit an application for participation in the grant program</h2>
+
+
+	<div class="d-flex flex-wrap forms">
+	    <div class="bx">
+            <a title="apply" href="#" target="_blank" class="page__btn page__btn--main getModal"> Legal entity </a>
+			<div class="modal">
+			    <div class="modal-overflow"></div>
+			    <div class="modal-container">
+			        <a href="javascript:;" class="modal-close"></a>
+			        <div class="modal-top">
+			            <div class="ttl">
+			                Legal entity </div>
+			        </div>
+			        <div class="modal-mid">
+			        	<script data-b24-form="inline/657/bd3fqd" data-skip-moving="true">
+						(function(w,d,u){
+						var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+						var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+						})(window,document,'https://team.alabuga.ru/upload/crm/form/loader_657_bd3fqd.js');
+						</script>
+			        </div>
+			    </div>
+			</div>
+
+	    </div>
+	    <div class="bx">
+            <a title="apply" href="#" target="_blank" class="page__btn page__btn--main getModal"> Individual </a>
+            <div class="modal">
+			    <div class="modal-overflow"></div>
+			    <div class="modal-container">
+			        <a href="javascript:;" class="modal-close"></a>
+			        <div class="modal-top">
+			            <div class="ttl">
+			                Individual </div>
+			        </div>
+			        <div class="modal-mid">
+			        	<script data-b24-form="inline/658/7pf2ep" data-skip-moving="true">
+						(function(w,d,u){
+						var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+						var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+						})(window,document,'https://team.alabuga.ru/upload/crm/form/loader_658_7pf2ep.js');
+						</script>
+			        </div>
+			    </div>
+			</div>
+	    </div>
+</div>
+</div>
---
+++ ru/grant_program.php
@@ -0,0 +1,270 @@
+<div class="main-program grant-program">
+<div class="page__wrap">
+
+	<h2 class="text-center">Грантовый конкурс "Алабуга Старт"</h2>
+
+	<div class="main-fields_block mb-10">
+	    <div class="d-flex flex-wrap justify-content-center">
+	        
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue ">
+	                        77 </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">
+	                            cтран принимают участие в программе "Алабуга Старт.
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue">
+	                        > 60 </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">представителей смогут получить поддержку от АО "ОЭЗ ППТ "Алабуга" за активную работу.</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx max-w-40" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name grant-program-numbs color-blue nowrap">
+	                        50 млн <span class="color-blue text-lg">рублей</span> </div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">Общий призовой фонд для участников грантового конкурса составляет 50 млн рублей.</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	    </div>
+	</div>
+
+
+	<h2 class="text-center">Номинации грантового конкурса</h2>
+
+
+	<div class="main-fields_block">
+	    <div class="d-flex flex-wrap justify-content-center mb-10">
+	        
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Мотивационная выплата:</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">Выплаты за трудоустройство кангдидатов на программу "Алабуга Старт"</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Номинация "Лидер трудоустройства":</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">Выплаты за сбор полного пакета документов для регистрации кандидата на программу</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Номинация "Полное сопровождение":</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">600 тыс. рублей средняя сумма выплаты одному представителю за выполнение условий двух номинаций</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	     </div>
+	    <div class="d-flex flex-wrap justify-content-center">
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Конкурсная выплата:</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">Единоразовая выплата одному представителю от региона за закрытие наибольшего процента от установленных рабочих мест в своем регионе в самые короткие сроки, но не менее 70%</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+	        <div class="bx" id="bx_1454625752_59">
+	            <div class="cart-fields">
+	                <div class="cart-fields_top">
+	                    <div class="name">Номинация "Партнер года" включающая IV степени гранта:</div>
+	                    <!-- /.name -->
+	                </div>
+	                <!-- /.cart-fields_top -->
+	                <div class="cart-fields_block">
+	                    <div class="group">
+	                        <div class="txt">До 1,2 млн. рублей одному представителю в рамках номинации "Партнер года"</div>
+	                    </div>
+	                </div>
+	            </div>
+	            <!-- /.cart-fields -->
+	        </div>
+
+
+	    </div>
+	</div>
+
+
+	<p class="notes my-10">* Суммы выплат зависят от количества квот, которые были выделены на вашу страну!</p>
+
+
+	<div class="d-flex flex-wrap">
+	    
+
+	    <div class="bx">
+	    	<div class="main-program_cart">
+	            <h2>Этапы грантового конкурса</h2>
+	            <div class="list">
+	                <ol class="grant-list pl-0">
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/telegram.svg" alt="Professional training" class="mw-100 d-block">
+	                        </div>
+	                       Прием заявок на участие в Грантовом конкурсе
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/laptop_icon.svg" alt="Russian language lessons" class="mw-100 d-block">
+	                        </div>
+	                        Работа партнеров в рамках конкурса
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/id-card_icon.svg" alt="Preferential accommodation in comfortable" class="mw-100 d-block">
+	                        </div>
+	                        Трудоустройство кандидатов, набранных партнерами
+	                    </li>
+	                    <li>
+	                        <div class="icon">
+	                            <img src="/local/templates/alabuga/img/icon/exam-results_icon.svg" alt="Preferential accommodation in comfortable" class="mw-100 d-block">
+	                        </div>
+	                        Оглашение результатов Грантового конкурса
+	                    </li>
+	                </ol>
+	            </div>
+	        </div>	
+	    </div>
+
+
+	    <div class="bx">
+	        <div class="main-program_cart">
+	            <h2>
+	                Документы </h2>
+	                <p>Подробную информацию о сроках и номинациях Грантового конкурса, количестве квот, условиях участия и проведении выплат см. в Положении о Грантовом конкурсе.</p>
+
+			        <div class="cart-fields_btn">
+		                <a target="_blank" href="/upload/Приказ_№316_от_10_03_2025_о_грантовом_конкурсе_Алабуга_Старт.pdf" class="page__btn page__btn--curr">
+		                    <span>Документы</span>
+		                </a>
+		            </div>
+
+	        </div>
+	    </div>
+
+
+	</div>
+
+
+	<h2 class="my-10">Форма заявки</h2>
+
+
+	<div class="d-flex flex-wrap forms">
+	    <div class="bx">
+            <a title="apply" href="#" target="_blank" class="page__btn page__btn--main getModal">Юридическое лицо</a>
+			<div class="modal">
+			    <div class="modal-overflow"></div>
+			    <div class="modal-container">
+			        <a href="javascript:;" class="modal-close"></a>
+			        <div class="modal-top">
+			            <div class="ttl">Юридическое лицо</div>
+			        </div>
+			        <div class="modal-mid">
+			        	<script data-b24-form="inline/657/bd3fqd" data-skip-moving="true">
+						(function(w,d,u){
+						var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+						var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+						})(window,document,'https://team.alabuga.ru/upload/crm/form/loader_657_bd3fqd.js');
+						</script>
+			        </div>
+			    </div>
+			</div>
+
+	    </div>
+	    <div class="bx">
+            <a title="apply" href="#" target="_blank" class="page__btn page__btn--main getModal"> Физическое лицо </a>
+            <div class="modal">
+			    <div class="modal-overflow"></div>
+			    <div class="modal-container">
+			        <a href="javascript:;" class="modal-close"></a>
+			        <div class="modal-top">
+			            <div class="ttl">
+			                Физическое лицо </div>
+			        </div>
+			        <div class="modal-mid">
+			        	<script data-b24-form="inline/658/7pf2ep" data-skip-moving="true">
+						(function(w,d,u){
+						var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+						var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+						})(window,document,'https://team.alabuga.ru/upload/crm/form/loader_658_7pf2ep.js');
+						</script>
+			        </div>
+			    </div>
+			</div>
+	    </div>
+</div>
+</div>
